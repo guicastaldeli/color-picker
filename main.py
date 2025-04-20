ee8156cb5805a11292827eda2711190c4caf7fab
gui castaldeli <guicastaldeli8@gmail.com>
2025-04-19 20:06:16 -0400

    update-00

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class Main:
+    #Window Configs
+    window = Tk()
+    window.title("Color Picker")
+    window.geometry("800x400")
+
+    #Init
+    def __init__(self):
+        Main.window.mainloop()
+
+main = Main()
+        
+    
